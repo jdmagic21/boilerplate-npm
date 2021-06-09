79c22d30f79123c79e45534cc70c659cc95cfdc2
Utt, Jason David <jutt@iu.edu>
2021-06-09 16:00:12 -0400

    move logic to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,50 @@
 var express = require('express');
 var app = express();
+const dotenv = require('dotenv'); 
+dotenv.config();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+// app.use(function(req, res, next){
+//   console.log(`${req.method} ${req.path} - ${req.ip}`);
+//   next(); 
+// }); 
+
+app.get(
+    "/now",
+    (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      res.send({
+        time: req.time
+      });
+    }
+  );
+  
+  app.get('/name',(req,res)=>{
+    res.json({name: `${req.query.first} ${req.query.last}`});
+  });
+  
+  app.get("/:word/echo", (req, res)=>{
+  res.json({echo:req.params.word});   
+  }); 
+  
+  
+  
+  app.get("/", function(req, res){
+    res.sendFile(__dirname + '/views/index.html')
+  });
+  
+  app.get('/json', function(req, res){
+    let message = "Hello json"; 
+    if(process.env.MESSAGE_STYLE == "uppercase"){
+      message = message.toUpperCase(); 
+    }
+  
+    res.json({"message": message}); 
+  });
 
 app.get('/', function(req, res){
     res.send("Hello Express");
--- server.js
+++ server.js
@@ -7,19 +7,8 @@ var bGround = require('fcc-express-bground');
 var myApp = require('./myApp');
 var express = require('express');
 var app = express();
-const dotenv = require('dotenv'); 
+
 const { param } = require('./myApp');
-dotenv.config();
-
-
-app.use('/public', express.static(__dirname + '/public'));
-
-// app.use(function(req, res, next){
-//   console.log(`${req.method} ${req.path} - ${req.ip}`);
-//   next(); 
-// }); 
-
-
 
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
@@ -34,41 +23,7 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-app.get(
-  "/now",
-  (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  },
-  (req, res) => {
-    res.send({
-      time: req.time
-    });
-  }
-);
 
-app.get('/name',(req,res)=>{
-  res.json({name: `${req.query.first} ${req.query.last}`});
-});
-
-app.get("/:word/echo", (req, res)=>{
-res.json({echo:req.params.word});   
-}); 
-
-
-
-app.get("/", function(req, res){
-  res.sendFile(__dirname + '/views/index.html')
-});
-
-app.get('/json', function(req, res){
-  let message = "Hello json"; 
-  if(process.env.MESSAGE_STYLE == "uppercase"){
-    message = message.toUpperCase(); 
-  }
-
-  res.json({"message": message}); 
-});
 
 
 var port = process.env.PORT || 3000;
