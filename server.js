e257fd809d4e581c1a5b4f4d90a7c34400956724
Utt, Jason David <jutt@iu.edu>
2021-06-09 14:54:30 -0400

    comment out logging

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ dotenv.config();
 
 app.use('/public', express.static(__dirname + '/public'));
 
-app.use(function(req, res, next){
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next(); 
-}); 
+// app.use(function(req, res, next){
+//   console.log(`${req.method} ${req.path} - ${req.ip}`);
+//   next(); 
+// }); 
 
 
 
